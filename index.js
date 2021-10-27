00fa44d1e5c8653bf63402e0815792591a2d555b
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-10-27 08:26:14 +0600

    new commit

--- index.js
+++ index.js
@@ -5,21 +5,28 @@ require("dotenv").config();
 const port=process.env.PORT || 5000
 const {MongoClient}=require('mongodb')
 
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.aicgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.aicgl.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 console.log(uri)
 // connect with mongodb
 async function run() {
   try {
     await client.connect();
+    
 console.log('database connected')
     const database = client.db('spread-hands');
     const events = database.collection('events');
+    const insertedData=await events.insertOne({name:'heroku'})
 
+    // load data
+    app.get('/events',async(req,res)=>{
+        const loadData=await events.find({}).toArray()
+        res.send(loadData)
+    })
 
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
