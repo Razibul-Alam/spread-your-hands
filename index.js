2b12c369351bacb10a9d1d40ccb770f9bf6ac907
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-10-27 07:18:49 +0600

    new commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express =require('express')
 const app =express()
 const cors =require('cors')
+require("dotenv").config();
 const port=process.env.Port || 5000
 
-
+// console.log(port)
 app.get('/',(req,res)=>{
     res.send('hello i am from node js')
 
